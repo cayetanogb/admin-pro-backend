62c477191fe92d82ad2d183682116cff89e18b27
cayetanogb22 <cayetanogb22@gmail.com>
2023-04-07 11:14:42 +0200

    Fin seccion 15

--- controllers/usaurios.js
+++ controllers/usaurios.js
@@ -76,6 +76,7 @@ const putUsuario = async (req, res = response) => {
 
     if (usuarioDB.email !== email) {
       const existeEmail = await Usuario.findOne({ email });
+
       if (existeEmail) {
         res.status(400).json({
           ok: false,
@@ -84,7 +85,15 @@ const putUsuario = async (req, res = response) => {
       }
     }
 
-    campos.email = email;
+    if (!usuarioDB.google) {
+      campos.email = email;
+    } else if (usuarioDB.email !== email) {
+      return res.status(400).json({
+        ok: false,
+        msg: "Usuario de google no pueden cambiar su correo",
+      });
+    }
+
     const usuarioActualizado = await Usuario.findByIdAndUpdate(uid, campos, {
       new: true,
     });
